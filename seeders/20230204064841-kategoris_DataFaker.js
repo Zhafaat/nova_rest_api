49583e77eda8c7d09a043b72270c2324121d2bde
zhafaat <ilmuumum18@gmail.com>
2023-05-18 14:04:29 +0800

    finish development

--- seeders/20230204064841-kategoris_DataFaker.js
+++ seeders/20230204064841-kategoris_DataFaker.js
@@ -7,14 +7,11 @@ const KategorisData = require('../data_faker/kategoris');
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    
-    
     const kategoris = new KategorisData()
     await queryInterface.bulkInsert('kategoris', kategoris.dataFaker, {});
   },
 
   async down (queryInterface, Sequelize) {
-
     await queryInterface.bulkDelete('kategoris', null, {});
   }
 };
